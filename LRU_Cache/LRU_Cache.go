fd28e2a5d308d82af0bfb712ec7b3c72c4f5819f
azabrs <Apukha@mail.ru>
2024-06-23 15:49:20 +0300

    add Mutex

--- LRU_Cache/LRU_Cache.go
+++ LRU_Cache/LRU_Cache.go
@@ -3,12 +3,14 @@ package lrucache
 import (
 	"container/list"
 	"fmt"
+	"sync"
 )
 
 type LRU_Cache struct{
 	capacity int
 	l *list.List
 	h map[interface{}] *list.Element
+	m *sync.RWMutex
 }
 
 func New(cap int) (LRU_Cache, error){
@@ -27,6 +29,8 @@ func (c LRU_Cache)Cap() int{
 }
 
 func (c LRU_Cache)Add(key, value interface{}){
+	c.m.Lock()
+	defer c.m.Unlock()
 	if node, ok := c.h[key]; !ok{
 		c.l.Remove(node)
 	}
@@ -38,6 +42,8 @@ func (c LRU_Cache)Add(key, value interface{}){
 }
 
 func (c LRU_Cache)Get(key interface{}) (value interface{}, ok bool){
+	c.m.Lock()
+	defer c.m.Unlock()
 	node, ok := c.h[key]
 	if !ok{
 		return nil, false
@@ -48,18 +54,24 @@ func (c LRU_Cache)Get(key interface{}) (value interface{}, ok bool){
 }
 
 func (c LRU_Cache)Clear(){
+	c.m.Lock()
+	defer c.m.Unlock()
 	for k := range c.h {
 		delete(c.h, k)
 	}
 	c.l.Init()
+	
 }
 
 func (c LRU_Cache)Remove(key interface{}) error{
+	c.m.Lock()
+	defer c.m.Unlock()
 	node, ok := c.h[key]
 	if !ok{
 		return fmt.Errorf("element does not exist")
 	}
 	c.l.Remove(node)
 	delete(c.h, key)
+	
 	return nil
 }
