50883dd2df17d06793f81faa28eb63ed19e5abb4
azabrs <Apukha@mail.ru>
2024-06-23 21:14:51 +0300

    add AddWithTLL method

--- LRU_Cache/LRU_Cache.go
+++ LRU_Cache/LRU_Cache.go
@@ -4,6 +4,7 @@ import (
 	"container/list"
 	"fmt"
 	"sync"
+	"time"
 )
 
 type LRU_Cache struct{
@@ -13,9 +14,10 @@ type LRU_Cache struct{
 	m sync.RWMutex
 }
 
-type Node struct{
+type NodeValue struct{
 	key interface{}
 	value interface{}
+	intCh chan struct{}
 }
 func New(cap int) (LRU_Cache, error){
 	if cap <= 0{
@@ -36,17 +38,20 @@ func (c *LRU_Cache)Add(key, value interface{}){
 	c.m.Lock()
 	defer c.m.Unlock()
 	if node, ok := c.h[key]; ok{
-		c.l.Remove(node)
+		
+		node := c.l.Remove(node)
+		close(node.(NodeValue).intCh)
 	}
-	node := c.l.PushBack(Node{
+	node := c.l.PushBack(NodeValue{
 		key: key,
 		value: value,
+		intCh: make(chan struct{}),
 	})
 	c.h[key] = node
 	if c.l.Len() > c.Cap(){
 		firstNode := c.l.Front()
 		c.l.Remove(firstNode)
-		delete(c.h, firstNode.Value.(Node).key)
+		delete(c.h, firstNode.Value.(NodeValue).key)
 	}
 }
 
@@ -59,7 +64,7 @@ func (c *LRU_Cache)Get(key interface{}) (value interface{}, ok bool){
 	}
 	c.l.MoveToBack(node)
 	
-	return node.Value.(Node).value, true
+	return node.Value.(NodeValue).value, true
 }
 
 func (c *LRU_Cache)Clear(){
@@ -83,4 +88,37 @@ func (c *LRU_Cache)Remove(key interface{}) error{
 	delete(c.h, key)
 	
 	return nil
+}
+
+func (c *LRU_Cache)AddWithTTL(key, value interface{}, ttl time.Duration){
+	c.m.Lock()
+	defer c.m.Unlock()
+	if node, ok := c.h[key]; ok{
+		node := c.l.Remove(node)
+		close(node.(NodeValue).intCh)
+	}
+	node := c.l.PushBack(NodeValue{
+		key: key,
+		value: value,
+		intCh: make(chan struct{}),
+	})
+	c.h[key] = node
+	if c.l.Len() > c.Cap(){
+		firstNode := c.l.Front()
+		c.l.Remove(firstNode)
+		delete(c.h, firstNode.Value.(NodeValue).key)
+	}
+	go func(ttl time.Duration, key interface{}){
+		
+		select{
+		case <-time.After(ttl):
+			c.m.Lock()
+			defer c.m.Unlock()
+			c.l.Remove(node)
+			delete(c.h, node.Value.(NodeValue).key)
+		case <- node.Value.(NodeValue).intCh:
+			return
+		}
+		
+	}(ttl, key)
 }
