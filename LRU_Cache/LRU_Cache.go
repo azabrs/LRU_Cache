8379ec282e04f8261c94de5c5a91c89699ef1b0a
azabrs <Apukha@mail.ru>
2024-06-23 15:18:54 +0300

    add Add method

--- LRU_Cache/LRU_Cache.go
+++ LRU_Cache/LRU_Cache.go
@@ -8,16 +8,41 @@ import (
 type LRU_Cache struct{
 	capacity int
 	l *list.List
-	h map[*list.Element] interface{}
+	h map[interface{}] *list.Element
+}
+
+type node struct{
+	key interface{}
+	value interface{}
 }
 
 func New(cap int) (LRU_Cache, error){
 	if cap <= 0{
-		return LRU_Cache{}, fmt.Errorf("Capacity must be positive")
+		return LRU_Cache{}, fmt.Errorf("capacity must be positive")
 	}
 	return LRU_Cache{
 		capacity: cap,
 		l: list.New(),
-		h: make(map[*list.Element]interface{}),
+		h: make(map[interface{}] *list.Element),
 	}, nil
 }
+
+func (c LRU_Cache)Cap() int{
+	return c.capacity
+}
+
+func (c LRU_Cache)Add(key, value interface{}){
+	if val, ok := c.h[key]; !ok{
+		c.l.Remove(val)
+	}
+	node := c.l.PushBack(node{
+		key: key,
+		value: value,
+	})
+	c.h[key] = node
+	if c.l.Len() > c.Cap(){
+		c.l.Remove(c.l.Front())
+	}
+}
+
+
